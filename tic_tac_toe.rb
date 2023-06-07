f0f33bd0d052dbaaf91a54cfe180bc563d41d85c
sherm_b <jeremy.steven.bess@gmail.com>
2023-06-07 15:07:58 -0700

    Create method to check if a space has been played

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,18 +1,21 @@
+# Handles drawing the board and board manipulations, as well as checks for win
+# state and cat's game.
 class Board
-  
   def initialize
-    @board_positions = Array.new(9) {|n| n+1}
+    @board_positions = Array.new(9) { |n| n + 1 }
   end
 
   def draw_board
-    puts <<-board 
+    puts <<-BOARD
      #{@board_positions[0]} | #{@board_positions[1]} | #{@board_positions[2]}
     ---+---+---
      #{@board_positions[3]} | #{@board_positions[4]} | #{@board_positions[5]}
     ---+---+---
      #{@board_positions[6]} | #{@board_positions[7]} | #{@board_positions[8]}
-    board
+    BOARD
+  end
+
+  def playable?(space)
+    space != 'X' || space != 'O'
   end
 end
-
-
